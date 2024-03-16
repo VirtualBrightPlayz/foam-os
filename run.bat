bda31351578133e119cad6daa6eef88e7e4b14d5
VirtualBrightPlayz <lego.trains.b@gmail.com>
2024-03-16 14:11:13 -0700

    update build script

--- run.bat
+++ run.bat
@@ -1,4 +1,7 @@
 @echo off
+mkdir "esp/"
+mkdir "esp/efi/"
+mkdir "esp/efi/boot/"
 cargo build --target x86_64-unknown-uefi &&^
 xcopy "target/x86_64-unknown-uefi/debug/foam-os.efi" "esp/efi/boot/bootx64.efi" /f /y &&^
 qemu-system-x86_64 -device virtio-rng-pci -drive if=pflash,format=raw,readonly=on,file=OVMF_CODE.fd -drive if=pflash,format=raw,readonly=on,file=OVMF_VARS.fd -drive format=raw,file=fat:rw:esp
